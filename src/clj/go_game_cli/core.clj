22aaa9f56fdcd204a73a09d660dc4e0666634318
J.R. Patten <jr@juanpatten.com>
2014-01-11 12:45:20 -0700

    Make CLI a little more friendly

--- src/clj/go_game_cli/core.clj
+++ src/clj/go_game_cli/core.clj
@@ -4,6 +4,9 @@
 (def CLI-PIECES {:black "O"
                 :white "@"})
 
+(def separators #"[-=/ ,]")
+(def coord-re (re-pattern (str "\\d+" separators "\\d+")))
+
 
 
 ;;;; Output
@@ -29,7 +32,7 @@
 ;;;; Input
 
 (defn- parse-user-coords [input]
-  (vec (map #(Integer/parseInt %) (clojure.string/split input #"[-=/ ,]"))))
+  (vec (map #(Integer/parseInt %) (clojure.string/split input separators))))
 
 (defn- prompt [color]
   (str (clojure.string/capitalize
@@ -37,16 +40,27 @@
        " " (CLI-PIECES color)
        " -- Make a move:"))
 
-(defn take-turns [board color]
+(defn take-turns [board color & [ko-history]]
   (println (print-board board))
   (println \newline (prompt color))
-  (let [input (read-line)]
+  (let [history (or ko-history #{})
+        input (read-line)]
     (cond
      (= "stop" input) (println "G'bye!")
-     (= "board" input) (do (println board) (recur board color))
+     (= "board" input) (do (println board) (recur board color [history]))
+     (= "hash" input) (do (println (goban/hash-board board)) (recur board color [history]))
+     (= "history" input) (do (println history) (recur board color [history]))
 
-     :else (let [coords (parse-user-coords input)
-                 new-board (goban/put-stone-at-point board color coords)]
-             (if new-board
-               (recur new-board (goban/next-color color))
-               (recur board color))))))
+     (re-matches coord-re input)
+     (let [coords (parse-user-coords input)
+           new-board (goban/place-stone board color coords history)]
+       (if-not new-board
+         (do (println \newline "INVALID MOVE" \newline)
+             (recur board color [history]))
+         (recur new-board
+                (goban/next-color color)
+                [(conj history (goban/hash-board new-board))])))
+
+     :else (do (println \newline "### I don't understand that input ###" \newline)
+               (recur board color [history])))))
+
